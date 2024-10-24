081bb1eb684704e6304456b9d4e5234b11fde8e5
Siva-0310 <sivachadalawada1923@gmail.com>
2024-10-24 10:04:52 +0530

    updated contact

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -3,59 +3,72 @@
 import { FC, useState } from "react";
 
 const Contact: FC = () => {
-    const [email, setEmail] = useState("");
-    const [subject, setSubject] = useState("");
-    const [message, setMessage] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
 
-    const handleSubmit = (e:React.FormEvent) => {
-        e.preventDefault()
-        if (!email || !subject || !message) {
-            alert("All fields are required!");
-            return;
-          }
-        console.log(email,subject,message)
-    }
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(email, subject, message);
+  };
 
   return (
-    <form className="flex flex-col w-[75%] sm:w-[60%] bg-card border border-border rounded-xl text-xm" onSubmit={handleSubmit}>
-        <div className="flex justify-center m-2">
-        <h1 className="font-semibold">Contact Us</h1>
-        </div>
+    <section className="flex flex-col w-[60%] items-center" id="contact">
+      <div className="flex flex-col justify-start my-10 w-full">
+        <h1 className="font-poppins text-lg font-semibold my-4">Get In Touch</h1>
+        <p className="text-sm">
+          Feel free to reach out if you’re interested in collaborating on a
+          project, sharing ideas, or just having a chat. I’m always open to
+          connecting with others!
+        </p>
+      </div>
+      <form
+        className="flex flex-col w-full bg-card border border-border rounded-xl text-sm"
+        onSubmit={handleSubmit}
+      >
         <div className="flex flex-col m-4 bg-grey">
-        <label className="font-semibold mb-4" htmlFor="email">Your Email Address</label>
-        <input
+          <input
             type="email"
             id="email"
-            placeholder="Enter your email"
-            className="p-3 rounded-xl focus:outline-none focus:border-blue-500 text-black"
-            onChange={(e) => {setEmail(e.target.value)}}
-        />
+            placeholder="Email"
+            className="bg-border p-3 rounded-xl focus:outline-none focus:ring-1 focus:ring-orange-500 text-white"
+            required
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
         </div>
         <div className="flex flex-col m-4">
-        <label className="font-semibold mb-4" htmlFor="subject">Message Subject</label>
-        <input
+          <input
             type="text"
             id="subject"
-            placeholder="Enter message subject"
-            className="p-3 rounded-xl focus:outline-none focus:border-blue-500 text-black"
-            onChange={(e) => {setSubject(e.target.value)}}
-        />
+            placeholder="Subject"
+            className="bg-border p-3 rounded-xl focus:outline-none focus:ring-1 focus:ring-orange-500 text-white"
+            required
+            onChange={(e) => {
+              setSubject(e.target.value);
+            }}
+          />
         </div>
         <div className="flex flex-col m-4">
-        <label className="font-semibold mb-4" htmlFor="body">Message Content</label>
-        <textarea
+          <textarea
             id="body"
-            placeholder="Write your message here"
-            className="p-3 rounded-xl focus:outline-none focus:border-blue-500 text-black"
-            onChange={(e) => {setMessage(e.target.value)}}
-        />
+            placeholder="Message"
+            className="bg-border p-3 rounded-xl focus:outline-none focus:ring-1 focus:ring-orange-500 text-black text-white"
+            required
+            onChange={(e) => {
+              setMessage(e.target.value);
+            }}
+            rows={5}
+          />
         </div>
         <div className="flex justify-center m-4">
-        <button className="py-2 px-5 rounded-xl bg-button">
+          <button className="py-2 px-5 rounded-xl bg-orange-400 focus:ring-1 focus:ring-orange-700">
             Send
-        </button>
+          </button>
         </div>
-    </form>
+      </form>
+    </section>
   );
 };
 
